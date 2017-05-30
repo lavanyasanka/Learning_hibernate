fa8eba9e4b9b8e68d5b877da09038c0d864c1156
lavanyasanka <lbillakanti@gmail.com>
2017-05-30 10:33:16 -0500

    update using hbm mapping file

---
+++ src/edu/lavanya/learn/hibernate/Daycare_request.java
@@ -0,0 +1,89 @@
+package edu.lavanya.learn.hibernate;
+
+import java.util.Date;
+
+public class Daycare_request {
+
+	private int ID_DAYCARE_REQUEST;
+	private Date DT_LAST_UPDATE;
+	private int ID_LAST_UPDATE_PERSON;
+	private Date DT_CREATED;
+	 private int ID_CREATED_PERSON;
+	 private int ID_CASE; 
+	 private int ID_STAGE; 
+	 private int ID_EVENT;
+	 private String IND_INVALID;
+	 private Date DT_INVALID;
+	 private String TXT_COMMENT_INVALID;
+	 
+	 //getters and setters methods
+	 
+	public int getID_DAYCARE_REQUEST() {
+		return ID_DAYCARE_REQUEST;
+	}
+	public void setID_DAYCARE_REQUEST(int iD_DAYCARE_REQUEST) {
+		ID_DAYCARE_REQUEST = iD_DAYCARE_REQUEST;
+	}
+	public Date getDT_LAST_UPDATE() {
+		return DT_LAST_UPDATE;
+	}
+	public void setDT_LAST_UPDATE(Date dT_LAST_UPDATE) {
+		DT_LAST_UPDATE = dT_LAST_UPDATE;
+	}
+	public int getID_LAST_UPDATE_PERSON() {
+		return ID_LAST_UPDATE_PERSON;
+	}
+	public void setID_LAST_UPDATE_PERSON(int iD_LAST_UPDATE_PERSON) {
+		ID_LAST_UPDATE_PERSON = iD_LAST_UPDATE_PERSON;
+	}
+	public Date getDT_CREATED() {
+		return DT_CREATED;
+	}
+	public void setDT_CREATED(Date dT_CREATED) {
+		DT_CREATED = dT_CREATED;
+	}
+	public int getID_CREATED_PERSON() {
+		return ID_CREATED_PERSON;
+	}
+	public void setID_CREATED_PERSON(int iD_CREATED_PERSON) {
+		ID_CREATED_PERSON = iD_CREATED_PERSON;
+	}
+	public int getID_CASE() {
+		return ID_CASE;
+	}
+	public void setID_CASE(int iD_CASE) {
+		ID_CASE = iD_CASE;
+	}
+	public int getID_STAGE() {
+		return ID_STAGE;
+	}
+	public void setID_STAGE(int iD_STAGE) {
+		ID_STAGE = iD_STAGE;
+	}
+	public int getID_EVENT() {
+		return ID_EVENT;
+	}
+	public void setID_EVENT(int iD_EVENT) {
+		ID_EVENT = iD_EVENT;
+	}
+	public String getIND_INVALID() {
+		return IND_INVALID;
+	}
+	public void setIND_INVALID(String iND_INVALID) {
+		IND_INVALID = iND_INVALID;
+	}
+	public Date getDT_INVALID() {
+		return DT_INVALID;
+	}
+	public void setDT_INVALID(Date dT_INVALID) {
+		DT_INVALID = dT_INVALID;
+	}
+	public String getTXT_COMMENT_INVALID() {
+		return TXT_COMMENT_INVALID;
+	}
+	public void setTXT_COMMENT_INVALID(String tXT_COMMENT_INVALID) {
+		TXT_COMMENT_INVALID = tXT_COMMENT_INVALID;
+	}
+	 
+	 
+}
--- src/edu/lavanya/learn/hibernate/test/DaycareRequest_hibernateTest.java
+++ src/edu/lavanya/learn/hibernate/test/DaycareRequest_hibernateTest.java
@@ -1,34 +1,61 @@
 package edu.lavanya.learn.hibernate.test;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import edu.lavanya.learn.hibernate.DaycareRequest_hibernate;
+
+import edu.lavanya.learn.hibernate.Daycare_request;
 
 public class DaycareRequest_hibernateTest {
 
 	public static void main(String[] args) {
+		SessionFactory sessionFactory = null;
+		try
+		{
+			sessionFactory = new Configuration().configure().buildSessionFactory();
 		
-		DaycareRequest_hibernate req1 = new DaycareRequest_hibernate();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			System.out.println("failed to create the Configuration and session factory");
+		}
+		Session session = sessionFactory.openSession();
+		Transaction trans = session.beginTransaction();
 		
-		 //req1.setDT_CREATED(todate('25/05/2017', 'DD/MM/YYYY'));
-		 req1.setID_LAST_UPDATE_PERSON(24813632);
+		try
+		{
+
+		
+			Daycare_request req1 = new Daycare_request();
+		
+		 req1.setDT_CREATED(new Date());
+		 req1.setID_LAST_UPDATE_PERSON(2271);
 		 req1.setID_CREATED_PERSON(24813632);
 		 req1.setID_CASE(25852854);
 		 req1.setID_STAGE(68326964);
 		 req1.setID_EVENT(266660193);
 		 req1.setIND_INVALID("Y");
 		 req1.setDT_INVALID(null);
-		 req1.setTXT_COMMENT_INVALID("something something something");
+		 req1.setTXT_COMMENT_INVALID("something something somethingmay 30");
 		 
-		 SessionFactory sessionfactory=new Configuration().configure().buildSessionFactory();
-			Session session=sessionfactory.openSession();
-			session.beginTransaction();
+		
 			session.save(req1);
 			//session.save(user2);
-			session.getTransaction().commit();
+			System.out.println("Daycare request got succesfully created..with id"+ req1.getID_DAYCARE_REQUEST());
+			
+			trans.commit();
+			session.close();
+			sessionFactory.close();
+			}catch(Exception ex)
+			{
+				trans.rollback();
+				ex.printStackTrace();
+			}
+			
+		}
 
 	}
-
-}
